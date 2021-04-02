1bd6161be149221136035902bf1fe4e83d1fa654
Matt Miller <mmiller@crispyecho.com>
2021-04-02 14:09:45 -0400

    Add comboboxes for selecting devices
    Add comboboxes for selecting loopback and webcam

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,9 @@
 #include <QByteArray>
 #include <QCheckBox>
 #include <QGroupBox>
+#include <QComboBox>
 #include <QVBoxLayout>
+#include <QLabel>
 
 #include <KTextEdit>
 #include <KLocalizedString>
@@ -33,9 +35,26 @@ QGroupBox* MainWindow::createOptionBox()
   QGroupBox *groupBox = new QGroupBox(tr("Options"));
   QCheckBox *flipInputVert = new QCheckBox("Flip Input Vertically");
   QCheckBox *setGreyscale = new QCheckBox("Greyscale");
+  QComboBox *setWebcam = new QComboBox(this);
+  QComboBox *setLoopback = new QComboBox(this);
+  //QStringList *device = new QStringList("/dev/video0");
+  QLabel *webcamLabel = new QLabel("Webcam:", this);
+  QLabel *loopbackLabel = new QLabel("Loopback device:", this);
+  setWebcam->insertItems(0, QStringList("/dev/video0"));
+  setLoopback->insertItems(0, QStringList("/dev/video2"));
+  webcamLabel->setBuddy(setWebcam);
+  loopbackLabel->setBuddy(setLoopback);
+  QHBoxLayout *hbox = new QHBoxLayout;
+  hbox->addWidget(webcamLabel);
+  hbox->addWidget(setWebcam);
+  QHBoxLayout *loopBox = new QHBoxLayout;
+  loopBox->addWidget(loopbackLabel);
+  loopBox->addWidget(setLoopback);
   QVBoxLayout *vbox = new QVBoxLayout;
   vbox->addWidget(flipInputVert);
   vbox->addWidget(setGreyscale);
+  vbox->addLayout(hbox);
+  vbox->addLayout(loopBox);
   groupBox->setLayout(vbox);
   return groupBox;
 }
