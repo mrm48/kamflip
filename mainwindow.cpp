73ee8046049df18b7df875515b55988c9960a3cf
Matt Miller <mmiller@crispyecho.com>
2021-04-02 14:27:30 -0400

    Remove unnecessary headers and UI elements
    Remove unnecessary text area UI headers and elements from ui.rc.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,20 +1,15 @@
 #include <QApplication>
 #include <QAction>
-#include <QSaveFile>
-#include <QFileDialog>
-#include <QTextStream>
-#include <QByteArray>
 #include <QCheckBox>
 #include <QGroupBox>
 #include <QComboBox>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QLabel>
 
-#include <KTextEdit>
 #include <KLocalizedString>
 #include <KActionCollection>
 #include <KStandardAction>
-#include <KMessageBox>
 #include <KIO/Job>
 
 #include "mainwindow.h"
